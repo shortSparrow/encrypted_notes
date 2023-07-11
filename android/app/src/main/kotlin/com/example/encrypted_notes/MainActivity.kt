c984b97d3830a001d709a0e25245dcffd1d3a5ac
Oleksandr Volkov <formobileprima@gmail.com>
2023-07-11 17:36:01 +0300

    init

---
+++ android/app/src/main/kotlin/com/example/encrypted_notes/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.encrypted_notes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home:  Scaffold(
+          appBar: AppBar(
+            title: const Text("Encrypted notes"),
+          ),
+          body: const Text("Hello")),
+    );
+  }
+}
